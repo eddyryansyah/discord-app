73445773a0d0a28e6225ad8094b23d8bd51f280a
Eddy Ryansyah <eddyryansyah1612@gmail.com>
2025-07-09 01:55:30 +0700

    Fix bot log message casing for consistency

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const {Client, GatewayIntentBits} = require('discord.js');
 require('dotenv/config');
 
 const client = new Client({
@@ -6,7 +6,7 @@ const client = new Client({
 });
 
 client.once('ready', () => {
-  console.log(`[client] Bot sudah online sebagai ${client.user.tag}`);
+  console.log(`[client] bot sudah online sebagai ${client.user.tag}`);
 });
 
 client.login(process.env.BOT_TOKEN);
